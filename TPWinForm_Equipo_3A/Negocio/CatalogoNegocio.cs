a63e528a949c7a799114821062a7c94db93e1c68
Juan Alvarez Ruiz <juandiegoalvarezruiz.083@gmail.com>
2026-04-26 18:51:39 -0300

    Nueva clase CatalogoNegocio

---
+++ TPWinForm_Equipo_3A/Negocio/CatalogoNegocio.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using TPWinForm_Equipo_3A.Dominio;
+
+namespace TPWinForm_Equipo_3A.Negocio
+{
+    public class CatalogoNegocio
+    {
+        public List<Marca> ListarMarcas()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT Id, Nombre FROM Marcas ORDER BY Nombre");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        public List<Categoria> ListarCategorias()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT Id, Nombre FROM Categorias ORDER BY Nombre");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        public void AgregarMarca(string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("INSERT INTO Marcas(Nombre) VALUES(@nombre)");
+                datos.SetearParametro("@nombre", nombre);
+                datos.EjecutarAccion();
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        public void AgregarCategoria(string nombre)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("INSERT INTO Categorias(Nombre) VALUES(@nombre)");
+                datos.SetearParametro("@nombre", nombre);
+                datos.EjecutarAccion();
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+    }
+}
